cc0c45c1145e30993b2ba56b7e8e087b4a443d2e
John <02501jm@gmail.com>
2018-12-06 11:48:01 -0600

    test

--- src/components/hero/heroComponent.js
+++ src/components/hero/heroComponent.js
@@ -6,7 +6,7 @@ class HeroComponent extends Component {
     return (
       <section className="main-section hero-header section--with-backdrop section--with-parallax">
         <h2 className="hero-header__subtitle">Hello there! I am</h2>
-        <h1 className="hero-header__title">John Miller</h1>
+        <h1 className="hero-header__title">John M.</h1>
         <h2 className="hero-header__subtitle">Scrum Master, HN</h2>
     </section>
     );
